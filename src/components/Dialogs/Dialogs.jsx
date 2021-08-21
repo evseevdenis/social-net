3782f3a685122e5a95df8b5671ea13855654592f
evseevdenis <79551366+evseevdenis@users.noreply.github.com>
2021-08-21 03:06:36 +0300

    edith path

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import DialogItem from "./DialogItem/DialogItem";
-import Message from "./Message/Message";
+import DialogItem from './Dialogitem/Dialogitem.jsx';
+import Message from "./message/Message";
 
 const Dialogs = (props) => {
 
