d8c1fb7a15396bb61273d24cbf7fc23b52f1a252
evseevdenis <79551366+evseevdenis@users.noreply.github.com>
2021-08-21 03:09:10 +0300

    Update Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import DialogItem from './Dialogitem/Dialogitem.jsx';
+import DialogItem from './Dialogitemw/Dialogitem.jsx';
 import Message from "./message/Message";
 
 const Dialogs = (props) => {
